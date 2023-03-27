c597cda63a67b347391925077e27cf62256e5162
Your Name <abdellatif.amira@gmail.com>
2023-03-27 10:30:36 +0200

    init project

---
+++ src/main/java/com/tachla/springboot/learnjpaandhebernate/LearnJpaAndHebernateApplication.java
@@ -0,0 +1,13 @@
+package com.tachla.springboot.learnjpaandhebernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHebernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHebernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tachla/springboot/learnjpaandhebernate/LearnJpaAndHebernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tachla.springboot.learnjpaandhebernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHebernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
